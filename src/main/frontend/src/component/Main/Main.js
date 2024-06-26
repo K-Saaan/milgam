55e4392c65e3c8d7b0c9c95d77fb2ec8b12b618b
been <ideal402@naver.com>
2024-06-26 13:28:27 +0900

    test. mui테스트

--- src/main/frontend/src/component/Main/Main.js
+++ src/main/frontend/src/component/Main/Main.js
@@ -1,15 +1,72 @@
-import React from 'react';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
+import React, { useState } from 'react';
+import { Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton, Box, Typography } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import InfoIcon from '@mui/icons-material/Info';
+
 
 const Main = () => {
-    return(
-        <Stack spacing={2} direction="row">
-           <Button variant="text">Text</Button>
-           <Button variant="contained">Contained</Button>
-           <Button variant="outlined">Outlined</Button>
-        </Stack>
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDrawer = (open) => (event) => {
+      if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+        return;
+      }
+      setIsOpen(open);
+    };
+
+    const drawerList = () => (
+      <Box
+        role="presentation"
+        onClick={toggleDrawer(false)}
+        onKeyDown={toggleDrawer(false)}
+        sx={{ width: 250 }}
+      >
+        <List>
+          {['Home', 'About', 'Contact'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index === 0 ? <HomeIcon /> : index === 1 ? <InfoIcon /> : <InfoIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+    );
+
+    return (
+      <Box sx={{
+        width: '100%',
+        minHeight: '100vh', // 뷰포트의 최소 높이를 100%로 설정
+        bgcolor: '#273142' // 배경색 설정
+      }}>
+        <Box sx={{
+          width: '100%',
+          height: 50,
+          bgcolor: '#456990', // 헤더의 배경색
+          color: 'white',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        }}>
+          <IconButton onClick={toggleDrawer(true)} sx={{ color: 'white', marginRight: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6">My Header</Typography>
+        </Box>
+        <Drawer
+          anchor='left'
+          open={isOpen}
+          onClose={toggleDrawer(false)}
+        >
+          {drawerList()}
+        </Drawer>
+        {/* 여기에 추가적인 컨텐츠를 배치할 수 있습니다. */}
+      </Box>
     );
 }
 
 export default Main;
+
+
