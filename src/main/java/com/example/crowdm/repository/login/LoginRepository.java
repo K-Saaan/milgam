d18773b2b3f295e27287ea897a5a661b42ea63e2
K-Saaan <111870436+K-Saaan@users.noreply.github.com>
2024-06-28 09:41:06 +0900

    feat. build.gradle update - web 통신을 위한 implementation 추가

--- src/main/java/com/example/crowdm/repository/login/LoginRepository.java
+++ src/main/java/com/example/crowdm/repository/login/LoginRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoginRepository extends JpaRepository<UserEntity, Integer>, JpaSpecificationExecutor<UserEntity> {
+
 }
