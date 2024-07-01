ab7262122863c4d791076f0b7f7911e13b3ce394
boreum <baenan1@naver.com>
2024-07-01 11:50:34 +0900

    refactor. 로그인 화면 리팩토링

---
+++ src/main/frontend/src/component/Styles/LongButton.js
@@ -0,0 +1,11 @@
+import Button from '@mui/material/Button';
+import { styled } from '@mui/system';
+
+const LongButton = styled(Button)({
+    marginTop: "20px",
+    width: "280px",
+    backgroundColor: "#4880FF",
+    padding: '10px',
+});
+
+export default LongButton;
