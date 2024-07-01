c74e1ac14034cea99181ef478bd4632034a7330a
been <ideal402@naver.com>
2024-07-01 15:52:20 +0900

    feat. 대시보드 알람DB 연결

--- src/main/java/com/example/crowdm/controller/dashboard/DashboardController.java
+++ src/main/java/com/example/crowdm/controller/dashboard/DashboardController.java
@@ -1,13 +1,43 @@
-package com.example.crowdm.controller.Dashboard;
+package com.example.crowdm.controller.dashboard;
 
+import com.example.crowdm.entity.dashboard.DashboardEntity;
+//import com.example.crowdm.repository.dashboard.DashboardRepository;
+import com.example.crowdm.service.dashboard.DashboardService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
 
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RequiredArgsConstructor
 @RestController
 public class DashboardController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final DashboardService dashboardService;
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @GetMapping("/dashboards")
+    public List<DashboardEntity> getAllDashboards() {
+        logger.info("Fetching all dashboards");
+        return dashboardService.findAllDashboards();
+    }
+
+    @DeleteMapping("/dashboards/delete{id}")
+    public int deleteDashboard(@PathVariable Integer id) {
+        logger.info("Deleting dashboard with id {}", id);
+        return dashboardService.deleteUser(id);
     }
 }
--- src/main/java/com/example/crowdm/service/dashboard/DashboardService.java
+++ src/main/java/com/example/crowdm/service/dashboard/DashboardService.java
@@ -1,2 +1,37 @@
-package com.example.crowdm.service.dashboard;public class DashboardService {
+package com.example.crowdm.service.dashboard;
+
+import com.example.crowdm.entity.dashboard.DashboardEntity;
+import com.example.crowdm.repository.dashboard.DashboardRepository;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DashboardService {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private final DashboardRepository dashboardRepository;
+    //final 변수: 초기화한 후 값을 변경될 수 없음
+
+    public List<DashboardEntity> findAllDashboards(){
+        return dashboardRepository.findAll();
+    }
+
+    @Transactional
+    public int deleteUser(Integer log_index){
+        try{
+            dashboardRepository.deleteById(log_index);
+            return 1;
+        }catch (Exception e){
+            logger.error("Error: {}", e.getMessage());
+            return 0;
+        }
+
+    }
 }
+
