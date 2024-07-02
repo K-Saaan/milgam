ca9deeda5702644e6465cb63f9bd68b036b8b139
boreum <baenan1@naver.com>
2024-07-02 18:47:53 +0900

    chore. 머지 후 라우터, 테마, 확장명 추가로 자잘한 수정

--- src/main/frontend/src/Router.js
+++ src/main/frontend/src/Router.js
@@ -61,6 +61,10 @@ const router = createBrowserRouter([
         element: <HostSignUp />,
       },
       {
+        path: "uploadVideo",
+        element: <UploadVideo />,
+      },
+      {
         path: "videoresult",
         element: <VideoResult />,
       },
--- src/main/frontend/src/routes/LogIn.js
+++ src/main/frontend/src/routes/LogIn.js
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import LogInForm from '../components/LogIn/LogInForm'
 
 function Login(){
     return (
       <>
-        <LogInForm/ >
+            <LogInForm/ >
       </>
     );
 }
