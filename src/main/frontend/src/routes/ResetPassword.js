178767e9a2a76bdadf3a69d70d587c384edc4420
boreum <baenan1@naver.com>
2024-07-03 16:36:31 +0900

    feat. 비밀번호 재설정 퍼블리싱

---
+++ src/main/frontend/src/routes/ResetPassword.js
@@ -0,0 +1,11 @@
+import ResetPasswordForm from '../components/LogIn/ResetPasswordForm'
+
+function ResetPassword(){
+    return (
+      <>
+        <ResetPasswordForm/ >
+      </>
+    );
+}
+
+export default ResetPassword
