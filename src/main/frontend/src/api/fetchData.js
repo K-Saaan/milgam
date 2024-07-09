ef86b371b333b47212501f00e9c4050523c75e92
mijin <mnmrtwg@gmail.com>
2024-07-09 17:08:50 +0900

    feat. 지역 선택 칸 생성 및 홈 라우터 설정

--- src/main/frontend/src/api/fetchData.js
+++ src/main/frontend/src/api/fetchData.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 const apiKey = process.env.REACT_APP_API_KEY;
 const apiUrl = 'http://openapi.seoul.go.kr:8088';
 
-export const fetchData = async () => {
+export const fetchData = async (region) => {
     try {
+      console.log('Fetching data for region:', region); // 콘솔 로그 추가
       const response = await axios.get(`${apiUrl}/${apiKey}/json/citydata_ppltn/1/2/광화문·덕수궁`);
       return response.data;
     } catch (error) {
