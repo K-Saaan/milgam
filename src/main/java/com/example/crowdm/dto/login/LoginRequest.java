60a5b583cca888b0f2c3f62bd94bf890bfa32f7f
K-Saaan <111870436+K-Saaan@users.noreply.github.com>
2024-07-09 09:54:16 +0900

    feat. loginRequest 추가

---
+++ src/main/java/com/example/crowdm/dto/login/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.example.crowdm.dto.login;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    private String id;
+    private String pw;
+}
