aad945d26e2d5a1e35efae1dfd399d8259c0ce16
bonjae <bonjae95@gmail.com>
2024-07-10 15:39:23 +0900

    fix. Front page3

--- src/main/frontend/src/components/Home/Page3.jsx
+++ src/main/frontend/src/components/Home/Page3.jsx
@@ -2,6 +2,7 @@ import { useTheme } from "@emotion/react";
 import { motion, useScroll, useTransform} from "framer-motion";
 import { useRef } from "react";
 import pic from "./pic.jpg"
+import { useForm } from "react-hook-form";
 
 // conatiner
 const cPage1Style = (theme) => ({
@@ -9,74 +10,47 @@ const cPage1Style = (theme) => ({
   hegiht:'100%',
   // background: theme.palette.comp,
   color : theme.palette.text.primary,
-  display: 'grid',
-  gridTemplateColumns: 'repeat(3, 1fr)', // 6열 그리드
-  gridTemplateRows: 'repeat(3, auto)',  // 각 열의 높이는 자동으로
-  gridGap: '10px',
-  gridTemplateAreas: `
-    "a . . "
-    ". b . "
-    ". . c "
-  `,
+  
 });
 
-const cDivAStyle =(theme) => ({ 
-  gridArea: 'a',
-  width: '100px',
-  height: '100px',
-  borderRadius: '10px',
-  border: '4px solid red',
-  overflow: 'hidden',
-  display:'flex',
-  justifyContent:'center'
- });
-
-// span
-const cSpanStyle = (theme) => ({
-  margin: '0',
-  color: 'red',
-  left: '10%',
-  fontSize: '56px',
-  fontWeight: '700',
-  letterSpacing: '-3px',
-  lineHeight: '1.2',
-  position: 'relative',
-});
-
-// text
-const cTextStyle = (theme) => ({
-  paddingRight:'12px',
-  display:'flex',
-  justifyContent:'center'
- });
- 
-const cDivCStyle = (theme) => ({
-  gridArea: 'c',
-  width: '100px',
-  height: '100px',
-  borderRadius: '10px',
-  background: 'red',
-  border: '4px solid red',
-  overflow: 'hidden',
-  display:'flex',
-  justifyContent:'center'
- });
-
-const cImgStyle = (theme) => ({
- width: '100px',
- height: '100px',
-});
 
 
 
 function Page3(){
 
+  const { register, handleSubmit, formState: { errors }, formState } = useForm();
+  const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+  
+  const onSubmit = async data => {
+    await sleep(2000);
+    if (data.username === "bill") {
+      alert(JSON.stringify(data));
+    } else {
+      alert("There is an error");
+    }
+  };
+
+
+  const Row1 = ({ columns }) => {
+    return (
+      <>
+        {columns.map((column) => (
+            <div>
+              <span key={column}>{column}</span>
+              <input {...register("username", { required: true })} />
+            </div>
+        ))}
+      </>  
+    );
+  };
+
+  // ############################################################################################
+
+  const columns = ['id', 'pw', 'name', 'email', 'phone', 'role_index', 'apply_date', 'account_lock', 'last_login', 'start_date', 'end_date']; 
+
   const theme = useTheme();
   const pageStyle = cPage1Style(theme);
-  const divAStyle = cDivAStyle(theme);
-  const divBStyle = cDivBStyle(theme);
-  const divCStyle = cDivCStyle(theme);
-  const imgCStyle = cImgStyle(theme);
+
   
 
   function useParallax(value, distance) {
@@ -100,23 +74,10 @@ function Page3(){
             }}
             style={pageStyle}
             >
+              <form onSubmit={handleSubmit(onSubmit)}>
 
-              <span style={spanStyle}>#003</span>
-
-              <div style={{...textStyle, y}}>
-               
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam ac
-                  rhoncus quam.
-                  Fringilla quam urna. Cras turpis elit, euismod eget ligula quis,
-                  imperdiet sagittis justo. In viverra fermentum ex ac vestibulum.
-                  Aliquam eleifend nunc a luctus porta. Mauris laoreet augue ut felis
-                  blandit, at iaculis odio ultrices. Nulla facilisi. Vestibulum cursus
-                  ipsum tellus, eu tincidunt neque tincidunt a.
-            
-              </div>
-
-              <img style={picStyle} src={pic} alt="giyomi" />
-
+                <Row1 columns={columns} />
+              </form>
 
             </motion.div>
           </>
