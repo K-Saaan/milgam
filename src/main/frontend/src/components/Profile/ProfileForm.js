5062e04b47b904b7bbf392291a01551f42217cce
YuByeongMin <qudals9928@naver.com>
2024-07-10 16:30:16 +0900

    feat: merge

--- src/main/frontend/src/components/Profile/ProfileForm.js
+++ src/main/frontend/src/components/Profile/ProfileForm.js
@@ -1,52 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import { Grid, Select, MenuItem, FormControl, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
+import { styled } from '@mui/system';
 import { CustomTypographyWrapper, CustomTypography } from './CustomTypo';
 import NewEvent from './NewEvent';
-import { CustomButton, NextButton } from "../SignUp/NextButton.js";
+import LongButton from "../Styles/LongButton.js";
+
+// 스크롤 안 보이게
+const noScrollbarStyles = {
+    '&::-webkit-scrollbar': {
+        display: 'none', // Chrome, Safari, and Opera
+    },
+    '-ms-overflow-style': 'none',  // Internet Explorer 10+
+    'scrollbar-width': 'none'  // Firefox
+};
+
+// 페이지 디자인
+const formSx = {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 'auto',
+    height: '75vh',
+    overflow: 'auto', // 스크롤 활성화
+    ...noScrollbarStyles // 스크롤 바 숨기기 스타일 추가
+};
+
+// 행사 선택 select box 스타일
+const EventControl = styled(FormControl)(({ theme }) => ({
+    width: '370px',
+    height: '56px',
+    backgroundColor: theme.palette.secondary.main,
+    '& .MuiOutlinedInput-root': {
+        '& fieldset': {
+            borderColor: theme.palette.border.primary,
+        },
+        '&:hover fieldset': {
+            borderColor: theme.palette.border.primary,
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: theme.palette.border.secondary,
+            borderWidth: '2px',
+        },
+    },
+}));
 
 const ProfileForm = ({ marginBottom }) => {
-
-    // 페이지 이동 부분 const
-    const navigate = useNavigate(); // 페이지 이동을 위한 navigate 함수
-    const location = useLocation(); // 현재 페이지 위치를 가져오기 위한 useLocation 함수
-
-    // 이전 페이지로 이동하기 위해 이전 위치를 저장합니다.
-    const from = location.state?.from || "/dashboard";
-
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [loadedEvents, setLoadedEvents] = useState([]); // 불러온 데이터 상태
+    const [customEvents, setCustomEvents] = useState([]);
+    const [event, setEvent] = useState('');
+    const [dialogOpen, setDialogOpen] = useState(false);
+    
+    const from = location.state?.from || "/dashboard"; // 이전 위치 저장
+    
     const onNextClick = () => {    // 이전 페이지로 이동하도록
         navigate(from);
     };
 
-    // 스크롤 안 보이게
-    const noScrollbarStyles = {
-        '&::-webkit-scrollbar': {
-            display: 'none', // Chrome, Safari, and Opera
-        },
-        '-ms-overflow-style': 'none',  // Internet Explorer 10+
-        'scrollbar-width': 'none'  // Firefox
-    };
-
-    // 페이지 디자인
-    const formSx = {
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        margin: 'auto',
-        height: '75vh',
-        overflow: 'auto', // 스크롤 활성화
-        ...noScrollbarStyles // 스크롤 바 숨기기 스타일 추가
-    };
-
-    // 이벤트 부분 const
-    const [event, setEvent] = useState('');
-    const [customEvents, setCustomEvents] = useState([]);
-    const [dialogOpen, setDialogOpen] = useState(false);
-
-    const handleEventChange = (event) => {
+    // 이벤트
+    const handleEventChange = (event) => { // 이벤트 선택
         const value = event.target.value;
         if (value === 'add-new') {
             setDialogOpen(true);
@@ -63,31 +80,39 @@ const ProfileForm = ({ marginBottom }) => {
         setDialogOpen(false);
     };
 
-    const handleDeleteEvent = (eventToDelete) => { // 직접 입력 후 생성된 이벤트 삭제
-        setCustomEvents(customEvents.filter(event => event !== eventToDelete));
-        if (event === eventToDelete) {
-            setEvent('');
+    // const handleDeleteEvent = (eventToDelete) => { // 직접 입력 후 생성된 이벤트 삭제
+    //     setCustomEvents(customEvents.filter(event => event !== eventToDelete));
+    //     if (event === eventToDelete) {
+    //         setEvent('');
+    //     }
+    // };
+    // 사용자 추가 데이터 삭제 핸들러
+    const handleDeleteCustomEvent = async (eventToDelete) => {
+        try {
+            await axios.delete(`/event/delete/${eventToDelete.id}`);
+            setCustomEvents(customEvents.filter(event => event.id !== eventToDelete.id));
+            if (event === eventToDelete.title) {
+                setEvent('');
+            }
+        } catch (error) {
+            console.error('이벤트 삭제 중 오류 발생:', error);
         }
     };
 
-    const selectStyles = {
-        backgroundColor : "#323D4E",
-        '& .MuiOutlinedInput-root': {
-            '& fieldset': {
-                borderColor: '#CFCFCF1D', // 기본 테두리 색상
-            },
-            '&:hover fieldset': {
-                borderColor: '#CFCFCF1D', // 호버 시 테두리 색상
-            },
-            '&.Mui-focused fieldset': {
-                borderColor: '#CFCFCF1D', // 포커스 시 테두리 색상
-                borderWidth: '2px', // 포커스 시 테두리 두께
-            },
-        },
-        width: '370px',
-        height: '56px',
-    }
-
+    useEffect(() => {
+        // 데이터를 가져오는 비동기 함수
+        const fetchEvents  = async () => {
+          try {
+            const response = await axios.get("/event/eventlist"); // 실제 API URL로 대체해야 합니다.
+            console.log('event 데이터:', response.data); // 데이터 확인을 위한 로그
+            setLoadedEvents(response.data);
+          } catch (error) {
+            console.error('event 데이터를 가져오는 중 오류 발생:', error);
+          }
+        };
+    
+        fetchEvents ();
+      }, []);
 
     return (
         <Grid
@@ -162,9 +187,7 @@ const ProfileForm = ({ marginBottom }) => {
                 <div style={{ marginBottom: '4px' }}>
                     행사
                 </div>
-                <FormControl 
-                    fullWidth
-                    sx={selectStyles}>
+                <EventControl fullWidth>
                     <Select
                         value={event}
                         onChange={handleEventChange}
@@ -179,20 +202,26 @@ const ProfileForm = ({ marginBottom }) => {
                         <MenuItem value="" disabled>
                             <em>선택</em>
                         </MenuItem>
-                        <MenuItem value="event1">Event 1</MenuItem>
-                        <MenuItem value="event2">Event 2</MenuItem>
+                        {loadedEvents.map((loadedEvent, index) => (
+                            <MenuItem
+                                key={index}
+                                value={loadedEvent.title}
+                            >
+                                {loadedEvent.title}
+                            </MenuItem>
+                        ))}
                         {customEvents.map((customEvent, index) => ( // 직접 입력된 행사명 생성 및 삭제
                             <MenuItem
                                 key={index}
-                                value={customEvent}
+                                value={customEvent.title}
                                 sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
                             >
-                                {customEvent}
+                                {customEvent.title}
                                 <IconButton
                                     size="small"
                                     onClick={(e) => {
                                         e.stopPropagation();
-                                        handleDeleteEvent(customEvent);
+                                        handleDeleteCustomEvent(customEvent);
                                     }}
                                 >
                                     <CloseIcon fontSize="small" />
@@ -201,10 +230,10 @@ const ProfileForm = ({ marginBottom }) => {
                         ))}
                         <MenuItem value="add-new">직접 입력</MenuItem>
                     </Select>
-                </FormControl>
+                </EventControl>
             </Grid>
             <Grid item xs={12} display={{ md: 'flex' }} justifyContent={{ md: 'center' }}>
-                <NextButton type="submit" onClick={onNextClick} text="완료"/> {/* 기존 페이지로 돌아감 */}
+                <LongButton type="submit" variant="contained" onClick={onNextClick}>완료</LongButton> {/* 기존 페이지로 돌아감 */}
             </Grid>
             <NewEvent open={dialogOpen} onClose={() => setDialogOpen(false)} onAddEvent={handleAddEvent} /> {/* NewEvent 팝업창 열림 */}
         </Grid>
