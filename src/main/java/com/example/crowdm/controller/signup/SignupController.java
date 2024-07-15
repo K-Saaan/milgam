015cc5679591a2c921107831f7f32ffc57ef686b
YuByeongMin <qudals9928@naver.com>
2024-07-15 13:40:52 +0900

    feat: sign_up

--- src/main/java/com/example/crowdm/controller/signup/SignupController.java
+++ src/main/java/com/example/crowdm/controller/signup/SignupController.java
@@ -5,10 +5,9 @@ import com.example.crowdm.service.signup.SignupService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
--- src/main/java/com/example/crowdm/service/signup/SignupService.java
+++ src/main/java/com/example/crowdm/service/signup/SignupService.java
@@ -1,7 +1,12 @@
 package com.example.crowdm.service.signup;
 
+import com.example.crowdm.dto.faq.UnlockList;
 import com.example.crowdm.dto.signup.SignUpDto;
+import com.example.crowdm.entity.user.UserEntity;
 import com.example.crowdm.repository.signup.SignupRepository;
+import com.example.crowdm.service.admin.SimplePasswordGenerator;
+import com.example.crowdm.service.mail.MailSender;
+import com.sun.tools.jconsole.JConsoleContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,6 +16,7 @@ public class SignupService {
 
     private final SignupRepository signupRepository;
     private final PasswordEncoder passwordEncoder;
+    static MailSender mailSender;
 
     @Autowired
     public SignupService(SignupRepository signupRepository, PasswordEncoder passwordEncoder) {
@@ -27,4 +33,54 @@ public class SignupService {
         signupRepository.save(signUpDto.toUserEntity());
         return signUpDto.getId();
     }
+
+//    public boolean sendVerificationCode(String verificationCode) {
+//        boolean result = false;
+//        if (verificationCode == null || verificationCode.isEmpty()) {
+//            return result;
+//        }
+//
+//        try {
+//            String temppw = SimplePasswordGenerator.generateRandomString(12);
+//            // DB에 임시번호 저장
+//            UserEntity userEntity = signupRepository.findByEmail(verificationCode);
+//
+//            if(userEntity != null) {
+//
+//            }
+//
+//            SignUpDto signUpDto = new SignUpDto();
+//            signUpDto.setTemppw(temppw);
+//
+//            System.out.println("Start emailService.sendTemporaryPassword >>>>>>>>>>>>>>>>>>>>> ");
+//            // 이메일 발송
+//            mailSender.sendMailTemppw(verificationCode, "temppw", temppw);
+//            result = true;
+//            System.out.println("Start emailService.sendTemporaryPassword >>>>>>>>>>>>>>>>>>>>> ");
+//        } catch (Exception e) {
+//            System.out.println("Error");
+//        }
+//        return result;
+//    }
+//
+//    public static boolean TempPwIsValid(String temppw) {
+//        boolean result = false;
+//        if (temppw == null || temppw.isEmpty()) {
+//            return result;
+//        }
+//
+//        try {
+//            SignUpDto signUpDto = new SignUpDto();
+//            String real = signUpDto.getTemppw();
+//
+//            if(real.equals(temppw)) {
+//                result = true;
+//            }
+//            System.out.println("Success!");
+//
+//        }catch (Exception e) {
+//            System.out.println("Error");
+//        }
+//        return result;
+//    }
 }
