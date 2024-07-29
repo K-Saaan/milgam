25cf9bc4597d5b18fdf456e18fb4f0d0e5c2b5bd
been <ideal402@naver.com>
2024-07-29 14:15:39 +0900

    fix. 메세지서비스 코드 복구

--- src/main/java/com/example/crowdm/service/message/MessageService.java
+++ src/main/java/com/example/crowdm/service/message/MessageService.java
@@ -10,6 +10,8 @@ import com.example.crowdm.repository.message.MessageLogRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import com.example.crowdm.event.message.MessageLogEvent;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,16 +24,27 @@ public class MessageService {
 
     private final MessageManageRepository messageManageRepository;
     private final MessageLogRepository messageLogRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
-    // 메니지먼트 dto 연결
+    /**
+     * 1. MethodName: getAllMessageManageEntities
+     * 2. ClassName : MessageService
+     * 3. Comment   : 메니지먼트 dto 연결
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
     public List<MessageManageDto> getAllMessageManageEntities() {
         List<MessageManageEntity> entities = messageManageRepository.findAll();
-        logger.info("entities:{}", entities.toString());
         List<MessageManageDto> dtos = entities.stream().map(this::convertToManageDto).collect(Collectors.toList());
-        logger.info("dtos:{}", dtos.toString());
         return dtos;
     }
-
+    /**
+     * 1. MethodName: convertToManageDto
+     * 2. ClassName : MessageService
+     * 3. Comment   : 엔티티를 dto로 변경
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
     public MessageManageDto convertToManageDto(MessageManageEntity entity) {
         MessageManageDto dto = new MessageManageDto();
         dto.setUserIndex(entity.getId().getUserIndex());
@@ -40,7 +53,13 @@ public class MessageService {
         dto.setVideoIndex(entity.getVideoIndex());
         return dto;
     }
-
+    /**
+     * 1. MethodName: convertToManageEntity
+     * 2. ClassName : MessageService
+     * 3. Comment   : dto를 엔티티로 변경
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
     public MessageManageEntity convertToManageEntity(MessageManageDto dto) {
         return MessageManageEntity.builder()
                 .id(new MessageManageId(dto.getUserIndex(), dto.getLogIndex()))
@@ -49,12 +68,25 @@ public class MessageService {
                 .build();
     }
 
-    // 메새지로그 dto 연결
+
+    /**
+     * 1. MethodName: getAllMessageLogEntities
+     * 2. ClassName : MessageService
+     * 3. Comment   : 메새지로그 dto 연결
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
     public List<MessageLogDto> getAllMessageLogEntities() {
         List<MessageLogEntity> entities = messageLogRepository.findAll();
         return entities.stream().map(this::convertToLogDto).collect(Collectors.toList());
     }
-
+    /**
+     * 1. MethodName: convertToLogDto
+     * 2. ClassName : MessageService
+     * 3. Comment   : 엔티티를 dto로 변경
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
     public MessageLogDto convertToLogDto(MessageLogEntity entity) {
         MessageLogDto logDto = new MessageLogDto();
         logDto.setLogIndex(entity.getLogIndex());
@@ -64,7 +96,13 @@ public class MessageService {
         logDto.setAnalysisIndex(entity.getAnalysisIndex());
         return logDto;
     }
-
+    /**
+     * 1. MethodName: convertToLogEntity
+     * 2. ClassName : MessageService
+     * 3. Comment   : dto를 엔티티로 변경
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
     public MessageLogEntity convertToLogEntity(MessageLogDto dto) {
         return MessageLogEntity.builder()
                 .logIndex(dto.getLogIndex())
@@ -75,14 +113,26 @@ public class MessageService {
                 .build();
     }
 
-    // 유저 인덱스 항목 가져오기
+
+    /**
+     * 1. MethodName: getMessagesByUserIndex
+     * 2. ClassName : MessageService
+     * 3. Comment   : 유저 인덱스 항목 가져오기
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
     public List<MessageManageDto> getMessagesByUserIndex(List<MessageManageDto> messages, long userIndex) {
         return messages.stream()
                 .filter(message -> message.getUserIndex() == userIndex)
                 .collect(Collectors.toList());
     }
-
-    // 로그인덱스 뽑아오기
+    /**
+     * 1. MethodName: getLogIndicesByUserIndex
+     * 2. ClassName : MessageService
+     * 3. Comment   : 로그인덱스 가져오기
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
     public List<Integer> getLogIndicesByUserIndex(List<MessageManageDto> messages, Integer userIndex) {
         return messages.stream()
                 .filter(message -> message.getUserIndex() == userIndex)
@@ -90,18 +140,48 @@ public class MessageService {
                 .collect(Collectors.toList());
     }
 
-    // 해당하는 log 가져오기
+
+    /**
+     * 1. MethodName: getMessageLogsByLogIndices
+     * 2. ClassName : MessageService
+     * 3. Comment   : logindex에 해당하는 log 가져오기
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
     public List<MessageLogDto> getMessageLogsByLogIndices(List<Integer> logIndices) {
         // 모든 메시지 로그 DTO를 가져옵니다.
         List<MessageLogDto> allDtos = getAllMessageLogEntities();
-        logger.info("allDTos:{}", allDtos);
         // DTO를 logIndices를 사용해 필터링합니다.
         return allDtos.stream()
                 .filter(dto -> logIndices.contains(dto.getLogIndex()))
                 .collect(Collectors.toList());
     }
 
-    // 메세지 로그 저장
+    /**
+     * 1. MethodName: updateMessageManage
+     * 2. ClassName : MessageService
+     * 3. Comment   : 메세지매니지 업데이트 메서드
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
+    public MessageManageDto updateMessageManage(MessageManageDto dto) {
+        MessageManageId id = new MessageManageId(dto.getUserIndex(), dto.getLogIndex());
+        MessageManageEntity entity = messageManageRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("No MessageManageEntity with given ID"));
+
+        entity.setConfirm(dto.isConfirm());
+
+        MessageManageEntity updatedEntity = messageManageRepository.save(entity);
+        return convertToManageDto(updatedEntity);
+    }
+
+    /**
+     * 1. MethodName: saveMessageLog
+     * 2. ClassName : MessageService
+     * 3. Comment   : 메세지 로그 저장
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
     public MessageLogDto saveMessageLog(MessageLogDto dto) {
         MessageLogEntity entity = convertToLogEntity(dto);
         MessageLogEntity savedEntity = messageLogRepository.save(entity);
@@ -111,7 +191,13 @@ public class MessageService {
 
         return savedDto;
     }
-    //메세지메니지 저장
+    /**
+     * 1. MethodName: saveMessageManage
+     * 2. ClassName : MessageService
+     * 3. Comment   : 메세지메니지 저장
+     * 4. 작성자    : been
+     * 5. 작성일    : 2024. 07. 26
+     **/
     public MessageManageDto saveMessageManage(MessageManageDto dto) {
         MessageManageEntity entity = convertToManageEntity(dto);
         MessageManageEntity savedEntity = messageManageRepository.save(entity);
